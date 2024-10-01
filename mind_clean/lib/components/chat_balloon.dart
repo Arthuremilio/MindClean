af7aa71b66d4d7626098f39418b4b89c2ab5cc3f
Arthuremilio <arthur.ecf@hotmail.com>
2024-10-01 15:04:28 -0300

    FEAT: Tela de chat

---
+++ mind_clean/lib/components/chat_balloon.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class ChatBalloon extends StatelessWidget {
+  final String message;
+  final bool isUserMessage;
+
+  const ChatBalloon({
+    Key? key,
+    required this.message,
+    required this.isUserMessage,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        mainAxisAlignment:
+            isUserMessage ? MainAxisAlignment.end : MainAxisAlignment.start,
+        children: [
+          if (isUserMessage) ...[
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.only(right: 8.0),
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primary,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Text(
+                  message,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+              ),
+            ),
+            CircleAvatar(
+              radius: 20,
+              backgroundImage: AssetImage(
+                  'assets/images/user_image.png'), // Substitua pelo caminho da imagem do usuário
+            ),
+          ] else ...[
+            CircleAvatar(
+              radius: 20,
+              backgroundImage: AssetImage(
+                  'lib/assets/img/mind_clean_logo.png'), // Substitua pelo caminho da imagem do usuário
+            ),
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.only(left: 8.0),
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primary,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Text(
+                  message,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+              ),
+            ),
+          ],
+        ],
+      ),
+    );
+  }
+}
--- mind_clean/lib/pages/chat_page.dart
+++ mind_clean/lib/pages/chat_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mind_clean/components/chat_balloon.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
@@ -6,18 +7,63 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('Mind Clean'),
-          actions: [
-            IconButton(
-              onPressed: () => {},
-              icon: Icon(Icons.logout),
-            )
-          ],
-        ),
-        body: Column(
-          children: [Text('MIND CLEAN')],
-        ));
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Mind Clean', style: Theme.of(context).textTheme.bodyLarge),
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        actions: [
+          IconButton(
+            onPressed: () => {},
+            icon: Icon(
+              Icons.logout,
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              reverse: true,
+              padding: const EdgeInsets.all(16.0),
+              children: [
+                ChatBalloon(
+                  message:
+                      'There are many programming languages in the market that are used in designing and building websites, various applications and other tasks. All these languages are popular in their place and in the way they are used, and many programmers learn and use them.',
+                  isUserMessage: false,
+                ),
+                ChatBalloon(
+                  message: 'User message here!',
+                  isUserMessage: true,
+                ),
+                // Adicione mais mensagens aqui, se necessário
+              ],
+            ),
+          ),
+          Divider(
+            color: Colors.white,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {},
+                  backgroundColor: Theme.of(context).colorScheme.secondary,
+                  child: const Icon(Icons.add, color: Colors.white),
+                ),
+                FloatingActionButton(
+                  onPressed: () {},
+                  backgroundColor: Theme.of(context).colorScheme.secondary,
+                  child: const Icon(Icons.camera_alt, color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
